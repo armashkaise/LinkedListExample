4e5e31ff5dd52c3790c952087dc40e6e24be021e
armashkaise <forts912@gmail.com>
2019-12-07 23:19:23 +0600

    Готово1

--- src/LinkedListPack/MyLinkedList.java
+++ src/LinkedListPack/MyLinkedList.java
@@ -5,44 +5,44 @@ public class MyLinkedList {
     private MyNode last;
     private int count;
 
-    public void add(int i) {
-        MyNode l = last;
-        MyNode f = first;
-        MyNode newNode = new MyNode(null, i, l);
+    public void add(int value) {
+        MyNode localLast = last;
+        //MyNode f = first;
+        MyNode newNode = new MyNode(null, value, localLast);
         last = newNode;
-        if (l == null){
+        if (localLast == null){
             first = newNode;
         }
         else {
-            l.next = newNode;
+            localLast.next = newNode;
         }
         count++;
     }
 
-    public void add(int index, int i) {
+    public void add(int index, int value) {
         if (index > count-1) {
             System.out.println("Заданый номер ячейки превышает количество имеющихся элементов");
             return;
         }
-        MyNode f = first;
-        MyNode l = null;
-        MyNode pred= null;
+        MyNode localFirst = first;
+        MyNode localLast = null;
+        MyNode predNode= null;
         for (int j = 0; j < count ; j++) {
             if (j == index){
-                if (f != null) l = f;
-                if (f.prev != null) pred = f.prev;
-                MyNode newNode = new MyNode(l, i,pred);
-                f.prev = newNode;
+                if (localFirst != null) localLast = localFirst;
+                if (localFirst.prev != null) predNode = localFirst.prev;
+                MyNode newNode = new MyNode(localLast, value,predNode);
+                localFirst.prev = newNode;
                 if (index == 0) {
                     first = newNode;
                 }
                 else {
-                    pred.next = newNode;
+                    predNode.next = newNode;
                 }
                 count++;
                 break;
             }
-            f = f.next;
+            localFirst = localFirst.next;
         }
     }
 
@@ -50,42 +50,42 @@ public class MyLinkedList {
         first = null;
     }
 
-    public void delete(int i){
-        MyNode f = first;
-        MyNode prev = null;
-        MyNode next = null;
+    public void delete(int value){
+        MyNode localFirst = first;
+        MyNode prevNode = null;
+        MyNode nextNode = null;
         for (int j = 0; j < count; j++){
-            if (f.item == i){
-                if (f.prev != null)
-                    prev = f.prev;
-                if (f.next != null)
-                    next = f.next;
-                MyNode newNode = new MyNode(next.next, next.item, prev);
-                if (newNode != null) prev.next = newNode;
+            if (localFirst.item == value){
+                if (localFirst.prev != null)
+                    prevNode = localFirst.prev;
+                if (localFirst.next != null)
+                    nextNode = localFirst.next;
+                MyNode newNode = new MyNode(nextNode.next, nextNode.item, prevNode);
+                if (newNode != null) prevNode.next = newNode;
                 count--;
             }
-            f = f.next;
+            localFirst = localFirst.next;
         }
     }
 
     public void print(){
         System.out.println("****************************************");
-        MyNode f = first;
+        MyNode localFirst = first;
         if (first == null){
             System.out.println("Значения не найдены");
             return;
         }
         for (int i = 0; i < count; i++) {
-                MyNode predNod = f.prev;
-                MyNode nextNod = f.next;
-                int item = f.item;
+                MyNode predNod = localFirst.prev;
+                MyNode nextNod = localFirst.next;
+                int item = localFirst.item;
             if (predNod != null) System.out.print("[" +  predNod.item + "] ");
             else System.out.print("[null] ");
             System.out.print(item);
             if (nextNod != null) System.out.print(" [" +  nextNod.item + "] ");
             else System.out.print(" [null]");
             System.out.println();
-            if (nextNod != null) f = nextNod;
+            if (nextNod != null) localFirst = nextNod;
         }
         System.out.println("****************************************");
     }
@@ -96,11 +96,11 @@ class MyNode{
     MyNode next;
     int item;
 
-    MyNode(MyNode first, int i, MyNode last){
+    MyNode(MyNode last , int value, MyNode first){
 
-        this.prev = last;
-        this.item = i;
-        this.next = first;
+        this.prev = first;
+        this.item = value;
+        this.next = last;
     }
 }
 
