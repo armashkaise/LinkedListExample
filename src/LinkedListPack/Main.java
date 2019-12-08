0ec8dfa5a79f22d337659480dd848f272b83115e
armashkaise <forts912@gmail.com>
2019-12-08 22:46:57 +0600

    Готово2

--- src/LinkedListPack/Main.java
+++ src/LinkedListPack/Main.java
@@ -21,6 +21,7 @@ public class Main {
         myLinkedList.delete();
         myLinkedList.print();
 
+
     }
 
 
--- src/LinkedListPack/MyLinkedList.java
+++ src/LinkedListPack/MyLinkedList.java
@@ -43,6 +43,7 @@ public class MyLinkedList {
                 break;
             }
             localFirst = localFirst.next;
+
         }
     }
 
